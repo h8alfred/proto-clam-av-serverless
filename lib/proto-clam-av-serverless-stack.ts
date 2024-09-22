6e818661311a9b29f8c06e380567cf3da9fb4815
Alfredo Navarro <h8alfred@gmail.com>
2024-09-23 00:05:15 +0800

    ClamAV infra using aws cdk.

---
+++ lib/proto-clam-av-serverless-stack.ts
@@ -0,0 +1,21 @@
+import * as cdk from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Stack, StackProps, RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { ServerlessClamscan } from 'cdk-serverless-clamscan';
+
+export class ProtoClamAvServerlessStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+        const sc = new ServerlessClamscan(this, 'rClamscan', {});
+        const bucket = new Bucket(this, 'rBucket', {
+            autoDeleteObjects: true,
+            removalPolicy: RemovalPolicy.DESTROY
+        });
+        sc.addSourceBucket(bucket);
+        new CfnOutput(this, 'oBucketName', {
+            description: 'The name of the input S3 Bucket',
+            value: bucket.bucketName
+        });
+    }
+}
